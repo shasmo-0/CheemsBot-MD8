c8fbdbe36edf2d5bc176b88967e544877ddecb95
Shasmo <115390236+shasmo-0@users.noreply.github.com>
2024-09-17 12:57:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['919400856482'] //ur owner number
+global.ownernomer = "918921037256" //ur owner number2
+global.ownername = "Shasmo" //ur owner name
+global.ytname = "YT: Shasmo" //ur yt chanel name
+global.socialm = "GitHub: Shasmo-0" //ur github or insta name
+global.location = "Neverland" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
+global.botname = "Bot"
+global.ownernumber = '919400856482'
+global.ownername = 'Shasmo'
+global.ownerNumber = ["919400856482@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@shasmo?si=Cx8gxriByuYbuzz8"
+global.websitex = "https://youtu.be/dQw4w9WgXcQ?si=PTGZGQwoR_DlBA9k"
 global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.wm = "Shasmo Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "Bot\n\n"
+global.creator = "918921037256@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
